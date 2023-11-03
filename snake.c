8d8c188208c65240af328327f13ff07198cdfc91
valonmulolli <valonmulolli@gmail.com>
2023-11-03 20:08:40 +0100

    game board

--- snake.c
+++ snake.c
@@ -1,4 +1,6 @@
 #include "snake.h"
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_rect.h>
 
 #if 0 
 //FULLSCREEN
@@ -14,6 +16,29 @@
 #define WINDOW_X 10
 #define WINDOW_Y -20
 #endif
+
+#define GRID_SIZE 20
+#define GRID_DIM 1000
+
+void render_grid(SDL_Renderer *renderer, int x, int y) {
+
+  SDL_SetRenderDrawColor(renderer, 0x55, 0x55, 0x55, 255);
+  int cell_size = GRID_DIM / GRID_SIZE;
+
+  SDL_Rect cell;
+  cell.w = cell_size;
+  cell.h = cell_size;
+
+  for (int i = 0; i < GRID_SIZE; i++) {
+    for (int j = 0; j < GRID_SIZE; j++) {
+      cell.x = x + (i * cell_size);
+      cell.y = y + (j * cell_size);
+      SDL_RenderDrawRect(renderer, &cell);
+    }
+  }
+
+  return;
+}
 
 int main() {
 
@@ -37,11 +62,43 @@ int main() {
     fprintf(stderr, "!renderer");
   }
 
-  SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 255);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
+  int grid_x = (WINDOW_WIDTH / 2) - (GRID_DIM / 2);
+  int grid_y = (WINDOW_HEIGHT / 2) - (GRID_DIM / 2);
 
-  SDL_Delay(2000);
+  // SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 255);
+  // SDL_RenderClear(renderer);
+  // SDL_RenderPresent(renderer);
+
+  bool quit = false;
+  SDL_Event event;
+
+  while (!quit) {
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+      case SDL_QUIT:
+        quit = true;
+        break;
+      case SDL_KEYUP:
+        break;
+      case SDL_KEYDOWN:
+        switch (event.key.keysym.sym) {
+        case SDLK_ESCAPE:
+          quit = true;
+          break;
+        }
+        break;
+      }
+    }
+
+    SDL_RenderClear(renderer);
+    // RENDER LOOP
+
+    render_grid(renderer, grid_x, grid_y);
+
+    // RENDER LOOP END
+    SDL_SetRenderDrawColor(renderer, 0x11, 0x11, 0x11, 255);
+    SDL_RenderPresent(renderer);
+  }
 
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
